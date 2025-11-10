f45ca034243f0f5fcd4e7fdf25ab6db2d1c0644d
Sanketchikane <sanketchikane2607@gmail.com>
2025-11-10 11:37:09 +0000

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,1 +1,11 @@
-test
+# routes/__init__.py
+from .auth_routes import auth_bp
+from .admin_routes import admin_bp
+from .file_routes import file_bp
+from .api_routes import api_bp
+
+def register_routes(app):
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(admin_bp)
+    app.register_blueprint(file_bp)
+    app.register_blueprint(api_bp)
